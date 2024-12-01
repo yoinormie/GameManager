23db82843098e66fff0c706f719cdb838f9298b9
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-01 18:32:48 +0100

    Primer commit

---
+++ src/main/java/com/yoi/GameManager/GameManagerApplication.java
@@ -0,0 +1,13 @@
+package com.yoi.GameManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoi/GameManager/GameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoi.GameManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
