d9bcd0dc8120623d86f6b58e2bb9aefe8a32f348
YoelGarciaLago <ygarcialago@danielcastelao.org>
2024-12-18 11:51:09 +0100

    Creada el objeto dotenv

--- src/main/java/com/yoi/GameManager/GameManagerApplication.java
+++ src/main/java/com/yoi/GameManager/GameManagerApplication.java
@@ -1,5 +1,6 @@
 package com.yoi.GameManager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GameManagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+//
+//		System.out.println("URL --> " + dotenv.get("DB_URL"));
+//		System.out.println("pssw --> " + dotenv.get("DB_PASSWORD"));
+//		System.out.println("user --> " + dotenv.get("DB_USER"));
 		SpringApplication.run(GameManagerApplication.class, args);
 	}
 
