5cf19fa4c09058ea282c5ee9ca5b90120fd5b5e1
YoelGarciaLago <ygarcialago@danielcastelao.org>
2024-12-18 12:22:03 +0100

    Falta que vayan las variables

--- src/main/java/com/yoi/GameManager/GameManagerApplication.java
+++ src/main/java/com/yoi/GameManager/GameManagerApplication.java
@@ -10,10 +10,6 @@ public class GameManagerApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
 
-//
-//		System.out.println("URL --> " + dotenv.get("DB_URL"));
-//		System.out.println("pssw --> " + dotenv.get("DB_PASSWORD"));
-//		System.out.println("user --> " + dotenv.get("DB_USER"));
 		SpringApplication.run(GameManagerApplication.class, args);
 	}
 
