dacb60b06e8647750b116dc98bfdae81e5c4b084
YoelGarciaLago <ygarcialago@danielcastelao.org>
2024-12-18 11:47:09 +0100

    Tablas de la app creadas

--- src/main/java/com/yoi/GameManager/Model/Entity/Game.java
+++ src/main/java/com/yoi/GameManager/Model/Entity/Game.java
@@ -1,11 +1,19 @@
 package com.yoi.GameManager.Model.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 
 @Entity
 @Table(name = "game")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Game {
 
     @Id
@@ -17,7 +25,9 @@ public class Game {
     private String developer;
     @Column(name = "publisher")
     private String publisher;
-    @JoinColumn(name = "owner")
-    private Owner owner;
+    @Column(name = "adquisitionDate")
     private Date adquisitionDate;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn (name = "owner")
+    private Owner owner;
 }
--- src/main/java/com/yoi/GameManager/Model/Entity/Owner.java
+++ src/main/java/com/yoi/GameManager/Model/Entity/Owner.java
@@ -1,9 +1,28 @@
 package com.yoi.GameManager.Model.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "owner")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Owner {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "first_surname")
+    private String first_surname;
+    @Column(name = "second_surname")
+    private String second_surname;
+
 }
