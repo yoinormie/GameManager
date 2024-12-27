9d13430900cfc00aff1084cedffa4666eae285e5
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-27 16:46:09 +0100

    Acabado el CRUD de owner en su service

--- src/main/java/com/yoi/GameManager/Services/OwnerService.java
+++ src/main/java/com/yoi/GameManager/Services/OwnerService.java
@@ -1,14 +1,15 @@
 package com.yoi.GameManager.Services;
 
+import com.yoi.GameManager.Exceptions.Owner.OwnerNotFound;
 import com.yoi.GameManager.Model.DTO.OwnerDTO;
 import com.yoi.GameManager.Model.Entity.Owner;
 import com.yoi.GameManager.Repositories.OwnerRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OwnerService {
@@ -26,8 +27,43 @@ public class OwnerService {
         return ResponseEntity.status(HttpStatus.OK).body(ownerDTOList);
     }
 
+    public ResponseEntity<OwnerDTO> getOwnerById(Long id){
+        Optional<Owner> gotten = ownerRepository.findById(id);
+
+        if(gotten.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(gotten
+                    .orElse(null)));
+        }
+
+        throw new OwnerNotFound(id, "El usuario no fue encontrado: ");
+        //return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(gotten));
+    }
+
     public ResponseEntity<OwnerDTO> createOwner(Owner owner){
         Owner saved = ownerRepository.save(owner);
         return ResponseEntity.status(HttpStatus.CREATED).body(new OwnerDTO(saved));
+    }
+
+    public ResponseEntity<OwnerDTO> updatedOwner(Long id, Owner owner){
+        Optional<Owner> gotten = ownerRepository.findById(id);
+        if(gotten.isPresent()){
+            Owner existingOwner = gotten.get();
+            existingOwner.setName(owner.getName());
+            existingOwner.setFirst_surname(owner.getFirst_surname());
+            existingOwner.setSecond_surname(owner.getSecond_surname());
+            ownerRepository.save(existingOwner);
+            return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(existingOwner));
+        }
+        throw new OwnerNotFound(id, "El usuario no fue encontrado: ");
+    }
+
+    public ResponseEntity deleteOwner(Long id){
+        Optional<Owner> gotten = ownerRepository.findById(id);
+        if(gotten.isPresent()){
+            Owner existingOwner = gotten.get();
+            ownerRepository.delete(existingOwner);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        throw new OwnerNotFound(id, "El usuario no existe: ");
     }
 }
