20606832736a29a853ddc3cbc9cb7225d66f0555
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-27 16:46:49 +0100

    Añadidos los endpoints y modificado los métodos por su service

--- src/main/java/com/yoi/GameManager/Controllers/OwnerController.java
+++ src/main/java/com/yoi/GameManager/Controllers/OwnerController.java
@@ -1,29 +1,55 @@
 package com.yoi.GameManager.Controllers;
 
+import com.yoi.GameManager.Model.DTO.OwnerDTO;
+import com.yoi.GameManager.Model.Entity.Owner;
+import com.yoi.GameManager.Services.OwnerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/owner")
 public class OwnerController {
 
-    @PostMapping
-    public ResponseEntity<String> createOwner(){
-        return ResponseEntity.status(HttpStatus.CREATED).body("created owner");
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
     }
 
-    @GetMapping
-    public ResponseEntity<String> getOwner(){
-        return ResponseEntity.status(HttpStatus.OK).body("got owner");    }
-
-    @PutMapping
-    public ResponseEntity<String> updateOwner(){
-        return ResponseEntity.status(HttpStatus.OK).body("updated owner");
+    @PostMapping()
+    public ResponseEntity<OwnerDTO> createOwner(@RequestBody Owner owner){
+        return ownerService.createOwner(owner);
     }
 
-    @DeleteMapping
-    public ResponseEntity<String> deleteOwner(){
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
+//    @GetMapping("/owner/{id}")
+//    public ResponseEntity<OwnerDTO> getOwner(Long id){
+//        //return ResponseEntity.status(HttpStatus.OK).body("got owner");
+//        return ownerService.getOwnerById(id);
+//        }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<OwnerDTO>> getAllOwners(){
+        return ownerService.getAllOwners();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OwnerDTO> updateOwner(@PathVariable Long id, @RequestBody Owner owner){
+        //return ResponseEntity.status(HttpStatus.OK).body("updated owner");
+        return ownerService.updatedOwner(id, owner);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OwnerDTO> getOwnerById(@PathVariable Long id){
+        return ownerService.getOwnerById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteOwner(@PathVariable Long id){
+        //return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
+        return ownerService.deleteOwner(id);
     }
 
 }
