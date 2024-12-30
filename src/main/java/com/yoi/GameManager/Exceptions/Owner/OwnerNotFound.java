d06c98baa738556917a8760d76383486f318f795
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-30 15:04:15 +0100

    Modificados los constructores

--- src/main/java/com/yoi/GameManager/Exceptions/Owner/OwnerNotFound.java
+++ src/main/java/com/yoi/GameManager/Exceptions/Owner/OwnerNotFound.java
@@ -1,5 +1,6 @@
 package com.yoi.GameManager.Exceptions.Owner;
 
+import com.yoi.GameManager.Exceptions.ErrorMessages;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -7,13 +8,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class OwnerNotFound extends RuntimeException {
 
     public OwnerNotFound() {
+        super(ErrorMessages.OWNER_NOT_FOUND.getMessage());
     }
 
-    public OwnerNotFound(String message) {
-        super(message);
-    }
-
-    public OwnerNotFound(Long id, String msg) {
-        super(msg + String.valueOf(id));
+    public OwnerNotFound(Long id) {
+        super(ErrorMessages.OWNER_NOT_FOUND.getMessage() + id);
     }
 }
