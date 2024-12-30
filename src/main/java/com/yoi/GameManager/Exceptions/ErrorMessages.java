36e6e4f1abd2528b06eddf67715e437112963a36
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-30 15:43:43 +0100

    Añadido espacio para mejorar legibilidad

--- src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
+++ src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorMessages {
-    OWNER_NOT_FOUND("El usuario no ha sido encontrado");
+    OWNER_NOT_FOUND("El usuario no ha sido encontrado ");
 
     private String message;
     //private int statusCode;
