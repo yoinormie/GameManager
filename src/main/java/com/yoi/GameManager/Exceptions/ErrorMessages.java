395b3139d85d6efe3d5465b8bb0b97f5df17add3
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-30 15:03:19 +0100

    Creada la enum para los mensajes de error

---
+++ src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
@@ -0,0 +1,18 @@
+package com.yoi.GameManager.Exceptions;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessages {
+    OWNER_NOT_FOUND("El usuario no ha sido encontrado");
+
+    private String message;
+    //private int statusCode;
+
+    ErrorMessages(String message) {
+        this.message = message;
+      //  this.statusCode = statusCode;
+    }
+
+
+}
