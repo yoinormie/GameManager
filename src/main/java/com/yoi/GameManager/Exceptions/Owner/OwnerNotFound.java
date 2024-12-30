497e8b8612a74879b026a6166284fe61ce52c1f6
YoelGarcíaLago <yoelgarcialago@gmail.com>
2024-12-30 14:11:06 +0100

    Aplicado el @ResponseStatus

--- src/main/java/com/yoi/GameManager/Exceptions/Owner/OwnerNotFound.java
+++ src/main/java/com/yoi/GameManager/Exceptions/Owner/OwnerNotFound.java
@@ -1,5 +1,9 @@
 package com.yoi.GameManager.Exceptions.Owner;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class OwnerNotFound extends RuntimeException {
 
     public OwnerNotFound() {
