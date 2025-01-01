741728a390cd659ab131a32df58852afefada204
YoelGarcíaLago <yoelgarcialago@gmail.com>
2025-01-01 11:48:05 +0100

    Documentado el controller con Swagger

--- src/main/java/com/yoi/GameManager/Controllers/OwnerController.java
+++ src/main/java/com/yoi/GameManager/Controllers/OwnerController.java
@@ -3,6 +3,7 @@ package com.yoi.GameManager.Controllers;
 import com.yoi.GameManager.Model.DTO.OwnerDTO;
 import com.yoi.GameManager.Model.Entity.Owner;
 import com.yoi.GameManager.Services.OwnerService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,33 +20,33 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
+    @Operation(summary = "Crea un nuevo owner")
     @PostMapping()
     public ResponseEntity<OwnerDTO> createOwner(@RequestBody Owner owner){
         return ownerService.createOwner(owner);
     }
 
-//    @GetMapping("/owner/{id}")
-//    public ResponseEntity<OwnerDTO> getOwner(Long id){
-//        //return ResponseEntity.status(HttpStatus.OK).body("got owner");
-//        return ownerService.getOwnerById(id);
-//        }
 
+    @Operation(summary = "Obtiene una lista de todos los owners de la base de datos")
     @GetMapping("/all")
     public ResponseEntity<List<OwnerDTO>> getAllOwners(){
         return ownerService.getAllOwners();
     }
 
+    @Operation(summary = "Modifica a un owner pasándole un id y el owner modificado")
     @PutMapping("/{id}")
     public ResponseEntity<OwnerDTO> updateOwner(@PathVariable Long id, @RequestBody Owner owner){
         //return ResponseEntity.status(HttpStatus.OK).body("updated owner");
         return ownerService.updatedOwner(id, owner);
     }
 
+    @Operation(summary = "Obtiene un owner por su id")
     @GetMapping("/{id}")
     public ResponseEntity<OwnerDTO> getOwnerById(@PathVariable Long id){
         return ownerService.getOwnerById(id);
     }
 
+    @Operation(summary = "Borra a un owner")
     @DeleteMapping("/{id}")
     public ResponseEntity deleteOwner(@PathVariable Long id){
         //return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
