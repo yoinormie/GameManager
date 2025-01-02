faba01549df68fd3c4213ceadab0f3f07c3ce286
YoelGarcíaLago <yoelgarcialago@gmail.com>
2025-01-02 16:11:07 +0100

    Creada la Entidad User y su DTO

---
+++ src/main/java/com/yoi/GameManager/Model/DTO/UserDTO.java
@@ -0,0 +1,18 @@
+package com.yoi.GameManager.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/yoi/GameManager/Model/Entity/User.java
@@ -0,0 +1,32 @@
+package com.yoi.GameManager.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Data
+@ToString
+@AllArgsConstructor
+@Document(collection = "Users")
+public class User {
+    @Id
+    private UUID id;
+
+    @Column(name = "name",nullable = false,length = 255)
+    private String name;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+}
