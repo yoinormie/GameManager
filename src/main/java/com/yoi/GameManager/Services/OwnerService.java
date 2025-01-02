1f905886f721c997b1f1ed5b91b9df3b47a1ee5b
YoelGarcíaLago <yoelgarcialago@gmail.com>
2025-01-02 12:22:06 +0100

    Modificado los getters/setters de los apellidos

--- src/main/java/com/yoi/GameManager/Services/OwnerService.java
+++ src/main/java/com/yoi/GameManager/Services/OwnerService.java
@@ -3,7 +3,7 @@ package com.yoi.GameManager.Services;
 import com.yoi.GameManager.Exceptions.Owner.OwnerNotFound;
 import com.yoi.GameManager.Model.DTO.OwnerDTO;
 import com.yoi.GameManager.Model.Entity.Owner;
-import com.yoi.GameManager.Repositories.OwnerRepository;
+import com.yoi.GameManager.Repositories.JPA.OwnerRepositoryJPA;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,21 +14,21 @@ import java.util.Optional;
 @Service
 public class OwnerService {
 
-    private final OwnerRepository ownerRepository;
+    private final OwnerRepositoryJPA ownerRepositoryJPA;
 
 
-    public OwnerService(OwnerRepository ownerRepository) {
-        this.ownerRepository = ownerRepository;
+    public OwnerService(OwnerRepositoryJPA ownerRepositoryJPA) {
+        this.ownerRepositoryJPA = ownerRepositoryJPA;
     }
 
 
     public ResponseEntity<List<OwnerDTO>> getAllOwners(){
-        List<OwnerDTO> ownerDTOList = ownerRepository.findAll().stream().map(OwnerDTO::new).toList();
+        List<OwnerDTO> ownerDTOList = ownerRepositoryJPA.findAll().stream().map(OwnerDTO::new).toList();
         return ResponseEntity.status(HttpStatus.OK).body(ownerDTOList);
     }
 
     public ResponseEntity<OwnerDTO> getOwnerById(Long id){
-        Optional<Owner> gotten = ownerRepository.findById(id);
+        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
         if(gotten.isPresent()){
             return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(gotten
                     .orElse(null)));
@@ -37,28 +37,28 @@ public class OwnerService {
     }
 
     public ResponseEntity<OwnerDTO> createOwner(Owner owner){
-        Owner saved = ownerRepository.save(owner);
+        Owner saved = ownerRepositoryJPA.save(owner);
         return ResponseEntity.status(HttpStatus.CREATED).body(new OwnerDTO(saved));
     }
 
     public ResponseEntity<OwnerDTO> updatedOwner(Long id, Owner owner){
-        Optional<Owner> gotten = ownerRepository.findById(id);
+        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
         if(gotten.isPresent()){
             Owner existingOwner = gotten.get();
             existingOwner.setName(owner.getName());
-            existingOwner.setFirst_surname(owner.getFirst_surname());
-            existingOwner.setSecond_surname(owner.getSecond_surname());
-            ownerRepository.save(existingOwner);
+            existingOwner.setFirstSurname(owner.getFirstSurname());
+            existingOwner.setSecondSurname(owner.getSecondSurname());
+            ownerRepositoryJPA.save(existingOwner);
             return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(existingOwner));
         }
         throw new OwnerNotFound(id);
     }
 
     public ResponseEntity deleteOwner(Long id){
-        Optional<Owner> gotten = ownerRepository.findById(id);
+        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
         if(gotten.isPresent()){
             Owner existingOwner = gotten.get();
-            ownerRepository.delete(existingOwner);
+            ownerRepositoryJPA.delete(existingOwner);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
         throw new OwnerNotFound(id);
