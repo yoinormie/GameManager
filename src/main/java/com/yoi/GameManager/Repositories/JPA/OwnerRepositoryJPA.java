eba6c3f258c9e06de3cedfaaac2a7b223abec3b2
YoelGarcíaLago <yoelgarcialago@gmail.com>
2025-01-06 15:18:22 +0100

    Repository de Owner actualizado

---
+++ src/main/java/com/yoi/GameManager/Repositories/JPA/OwnerRepositoryJPA.java
@@ -0,0 +1,10 @@
+package com.yoi.GameManager.Repositories.JPA;
+
+import com.yoi.GameManager.Model.Entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepositoryJPA extends JpaRepository<Owner,Long> {
+
+}
