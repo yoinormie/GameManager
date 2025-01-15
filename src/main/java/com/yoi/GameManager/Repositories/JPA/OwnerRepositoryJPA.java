4ead7a934261cb0b8946c6d8775e9a9084daef82
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-15 11:08:53 +0100

    Id del repository modificado

--- src/main/java/com/yoi/GameManager/Repositories/JPA/OwnerRepositoryJPA.java
+++ src/main/java/com/yoi/GameManager/Repositories/JPA/OwnerRepositoryJPA.java
@@ -4,7 +4,9 @@ import com.yoi.GameManager.Model.Entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface OwnerRepositoryJPA extends JpaRepository<Owner,Long> {
+public interface OwnerRepositoryJPA extends JpaRepository<Owner, UUID> {
 
 }
