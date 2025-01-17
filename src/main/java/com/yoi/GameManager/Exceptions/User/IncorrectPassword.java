43415caf335bcf8554fc89808a029371881909c4
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-17 13:01:52 +0100

    Nueva excepción de User

---
+++ src/main/java/com/yoi/GameManager/Exceptions/User/IncorrectPassword.java
@@ -0,0 +1,9 @@
+package com.yoi.GameManager.Exceptions.User;
+
+import com.yoi.GameManager.Exceptions.ErrorMessages;
+
+public class IncorrectPassword extends RuntimeException {
+    public IncorrectPassword() {
+        super(ErrorMessages.INCORRECT_PASSWORD.getMessage());
+    }
+}
