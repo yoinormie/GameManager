601ecb65f32fda08f0d0dcc5ba0bf00dde8da7f4
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-17 13:02:05 +0100

    Nuevo mensaje de error

--- src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
+++ src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorMessages {
     USER_NOT_VALID("El usuario no es válido"),
     USER_NOT_FOUND("El usuario no ha sido encontrado"),
+    INCORRECT_PASSWORD("La contraseña que se ha proporcionado no es correcta"),
     UNIDENTIFIED_USER("El usuario que has puesto no ha sido identificado"),
     OWNER_NOT_FOUND("El propietario no ha sido encontrado "),
     UNABLE_TO_INSTANCE("Esta clase no es posible de instanciar ");
