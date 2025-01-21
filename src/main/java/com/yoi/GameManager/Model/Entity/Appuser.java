e8b191965fe35ddac67ed786911d1870ace11ac3
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-21 11:15:25 +0100

    Añadido @Field para guardar el UUID en MongoDB

--- src/main/java/com/yoi/GameManager/Model/Entity/Appuser.java
+++ src/main/java/com/yoi/GameManager/Model/Entity/Appuser.java
@@ -1,5 +1,6 @@
 package com.yoi.GameManager.Model.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import jakarta.persistence.*;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +26,10 @@ public class Appuser {
     @Column(name = "id_user", nullable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id_user;
+
+    @Field
+    @Transient
+    private String mongo_id_user;
 
     @Column(name = "username",nullable = false,length = 255)
     private String username;
