3d1b8b67f6ed9d6c3cde5181d01ba1d8bad4b45e
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-28 13:45:33 +0100

    Nuevo EndPoint para modificar el correo de un usuario existente

--- src/main/java/com/yoi/GameManager/Controllers/AppuserController.java
+++ src/main/java/com/yoi/GameManager/Controllers/AppuserController.java
@@ -1,8 +1,9 @@
 package com.yoi.GameManager.Controllers;
 
 import com.yoi.GameManager.Model.DTO.EntityDTOs.AppuserDTO;
-import com.yoi.GameManager.Model.DTO.RequestDTOs.DeleteUserDTO;
-import com.yoi.GameManager.Model.DTO.RequestDTOs.ModifyUsernameDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.AppuserRequests.DeleteUserDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.AppuserRequests.ModifyUserEmailDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.AppuserRequests.ModifyUsernameDTO;
 import com.yoi.GameManager.Model.Entity.JPA.Appuser;
 import com.yoi.GameManager.Services.AppuserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -41,5 +42,10 @@ public class AppuserController {
     @DeleteMapping("/{username}")
     public ResponseEntity deleteUser(@RequestBody DeleteUserDTO request){
         return appuserService.deleteUser(request);
+    }
+
+    @PutMapping("/newEmail/{username}")
+    public ResponseEntity<AppuserDTO> updateTheEmail(@RequestBody ModifyUserEmailDTO request){
+        return appuserService.modifyTheEmail(request);
     }
 }
