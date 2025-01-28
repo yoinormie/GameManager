fa9949c2563ec304dd4b29e9463a8d3ee5d83a13
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-28 13:44:44 +0100

    Comentada la clase + método para verificar los correos

--- src/main/java/com/yoi/GameManager/Utilities/DatabaseUtils.java
+++ src/main/java/com/yoi/GameManager/Utilities/DatabaseUtils.java
@@ -3,6 +3,8 @@ package com.yoi.GameManager.Utilities;
 
 import java.util.UUID;
 
+import com.yoi.GameManager.Repositories.JPA.AppuserRepositoryJPA;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
@@ -13,10 +15,13 @@ public final class DatabaseUtils {
 //        return UUID.randomUUID();
 //    }
 
+    AppuserRepositoryJPA appuserRepositoryJPA ;
+    @Operation(summary = "Genera una contraseña hasheada para guardarla en las bases de datos")
     public String generateHashedPassword(String password){
         return BCrypt.hashpw(password,BCrypt.gensalt());
     }
 
+    @Operation(summary = "Verifica a través de una expresión regular si el correo es válido")
     public boolean verifyValidEmail(String email){
         if(email == null || email.isBlank()){
             return false;
@@ -24,7 +29,13 @@ public final class DatabaseUtils {
         return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     }
 
+    @Operation(summary = "Verifica si la contraseña insertada y la que está guardada coinciden")
     public boolean verifyInsertedPassword(String insertedPassword, String checkedPassword){
         return BCrypt.checkpw(insertedPassword,checkedPassword);
+    }
+
+    @Operation(summary = "Verifica si el correo insertado es igual al almacenado")
+    public boolean verifyInsertedEmail(String insertedEmail, String checkedEmail){
+        return appuserRepositoryJPA.findByEmail(insertedEmail).get().getEmail().equals(checkedEmail);
     }
 }
