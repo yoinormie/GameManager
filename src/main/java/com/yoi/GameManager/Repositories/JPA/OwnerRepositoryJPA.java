8d0162acab6f54ead37736f118e4b4640b315d29
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-31 12:11:07 +0100

    Método para buscar por nombre

--- src/main/java/com/yoi/GameManager/Repositories/JPA/OwnerRepositoryJPA.java
+++ src/main/java/com/yoi/GameManager/Repositories/JPA/OwnerRepositoryJPA.java
@@ -4,9 +4,10 @@ import com.yoi.GameManager.Model.Entity.JPA.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OwnerRepositoryJPA extends JpaRepository<Owner, UUID> {
-
+    Optional<Owner> findByName(String name);
 }
