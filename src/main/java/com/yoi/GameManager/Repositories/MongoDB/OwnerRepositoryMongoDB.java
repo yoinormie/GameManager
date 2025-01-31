1d224c68360a03e00ca881a290f92386dcef9010
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-31 13:59:43 +0100

    Modificado la entidad objetivo

--- src/main/java/com/yoi/GameManager/Repositories/MongoDB/OwnerRepositoryMongoDB.java
+++ src/main/java/com/yoi/GameManager/Repositories/MongoDB/OwnerRepositoryMongoDB.java
@@ -1,11 +1,12 @@
 package com.yoi.GameManager.Repositories.MongoDB;
 
 import com.yoi.GameManager.Model.Entity.JPA.Owner;
+import com.yoi.GameManager.Model.Entity.MongoDB.OwnerMongoDB;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface OwnerRepositoryMongoDB extends MongoRepository<Owner, UUID> {
+public interface OwnerRepositoryMongoDB extends MongoRepository<OwnerMongoDB, UUID> {
 }
