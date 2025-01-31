408988568507fd2aa396aae4a0973742c7c2a2e6
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-01-31 13:59:27 +0100

    Creado el create y el update

--- src/main/java/com/yoi/GameManager/Services/OwnerService.java
+++ src/main/java/com/yoi/GameManager/Services/OwnerService.java
@@ -1,66 +1,54 @@
 package com.yoi.GameManager.Services;
 
+import com.yoi.GameManager.Exceptions.Owner.InvalidOwner;
 import com.yoi.GameManager.Exceptions.Owner.OwnerNotFound;
-import com.yoi.GameManager.Model.DTO.OwnerDTO;
-import com.yoi.GameManager.Model.Entity.Owner;
+import com.yoi.GameManager.Model.DTO.EntityDTOs.OwnerDTO;
+import com.yoi.GameManager.Model.Entity.JPA.Owner;
+import com.yoi.GameManager.Model.Entity.MongoDB.OwnerMongoDB;
 import com.yoi.GameManager.Repositories.JPA.OwnerRepositoryJPA;
+import com.yoi.GameManager.Repositories.MongoDB.OwnerRepositoryMongoDB;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class OwnerService {
 
     private final OwnerRepositoryJPA ownerRepositoryJPA;
+    private final OwnerRepositoryMongoDB ownerRepositoryMongoDB;
 
-
-    public OwnerService(OwnerRepositoryJPA ownerRepositoryJPA) {
+    public OwnerService(OwnerRepositoryJPA ownerRepositoryJPA, OwnerRepositoryMongoDB ownerRepositoryMongoDB) {
         this.ownerRepositoryJPA = ownerRepositoryJPA;
+        this.ownerRepositoryMongoDB = ownerRepositoryMongoDB;
     }
-
-
-    public ResponseEntity<List<OwnerDTO>> getAllOwners(){
-        List<OwnerDTO> ownerDTOList = ownerRepositoryJPA.findAll().stream().map(OwnerDTO::new).toList();
-        return ResponseEntity.status(HttpStatus.OK).body(ownerDTOList);
-    }
-
-//    public ResponseEntity<OwnerDTO> getOwnerById(Long id){
-//        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
-//        if(gotten.isPresent()){
-//            return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(gotten
-//                    .orElse(null)));
-//        }
-//        throw new OwnerNotFound(id);
-//    }
 
     public ResponseEntity<OwnerDTO> createOwner(Owner owner){
-        Owner saved = ownerRepositoryJPA.save(owner);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new OwnerDTO(saved));
+        if(validarCreacionOwner(owner))
+            return guardarOwnerEnBBDD(owner);
+        throw new InvalidOwner();
     }
 
-//    public ResponseEntity<OwnerDTO> updatedOwner(Long id, Owner owner){
-//        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
-//        if(gotten.isPresent()){
-//            Owner existingOwner = gotten.get();
-//            existingOwner.setName(owner.getName());
-//            existingOwner.setFirstSurname(owner.getFirstSurname());
-//            existingOwner.setSecondSurname(owner.getSecondSurname());
-//            ownerRepositoryJPA.save(existingOwner);
-//            return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(existingOwner));
-//        }
-//        throw new OwnerNotFound(id);
-//    }
 
-//    public ResponseEntity deleteOwner(Long id){
-//        Optional<Owner> gotten = ownerRepositoryJPA.findById(id);
-//        if(gotten.isPresent()){
-//            Owner existingOwner = gotten.get();
-//            ownerRepositoryJPA.delete(existingOwner);
-//            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-//        }
-//        throw new OwnerNotFound(id);
-//    }
+    public ResponseEntity<OwnerDTO> getOwnerByName(String name){
+        if(!name.isBlank()){
+            Owner search = ownerRepositoryJPA.findByName(name).get();
+            return ResponseEntity.status(HttpStatus.OK).body(new OwnerDTO(search));
+        }
+        throw new OwnerNotFound();
+    }
+
+    private ResponseEntity<OwnerDTO> guardarOwnerEnBBDD(Owner owner) {
+        ownerRepositoryJPA.save(owner);
+        OwnerMongoDB ownerDocument = OwnerMongoDB.newOwner(owner);
+        ownerRepositoryMongoDB.save(ownerDocument);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new OwnerDTO(owner));
+    }
+
+    private static boolean validarCreacionOwner(Owner owner) {
+        return comprobarNombresOwner(owner) && !owner.getUserId().getId_user().toString().isBlank();
+    }
+
+    private static boolean comprobarNombresOwner(Owner owner) {
+        return !owner.getName().isBlank() && !owner.getFirstSurname().isBlank() && !owner.getSecondSurname().isBlank();
+    }
 }
