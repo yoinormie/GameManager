ef5c7692ccd48252fbb510239166952d4f4641a2
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-02-04 13:22:47 +0100

    Modificado el ErrorMessage

--- src/main/java/com/yoi/GameManager/Exceptions/Appuser/UserNotFound.java
+++ src/main/java/com/yoi/GameManager/Exceptions/Appuser/UserNotFound.java
@@ -4,6 +4,6 @@ import com.yoi.GameManager.Exceptions.ErrorMessages;
 
 public class UserNotFound extends RuntimeException {
     public UserNotFound() {
-        super(ErrorMessages.UNIDENTIFIED_USER.getMessage());
+        super(ErrorMessages.USER_NOT_FOUND.getMessage());
     }
 }
