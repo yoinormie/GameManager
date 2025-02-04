bfa4f014a6c87e98c9236a219fe646947d5e22ca
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-02-04 13:23:13 +0100

    [PROVISIONAL] Añadido nuevo mensaje de error

--- src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
+++ src/main/java/com/yoi/GameManager/Exceptions/ErrorMessages.java
@@ -8,6 +8,7 @@ public enum ErrorMessages {
     USER_NOT_FOUND("El usuario no ha sido encontrado"),
     INCORRECT_PASSWORD("La contraseña que se ha proporcionado no es correcta"),
     UNIDENTIFIED_USER("El usuario que has puesto no ha sido identificado"),
+    UNABLE_TO_DESERIALIZE_USER("El user no ha podido ser deserializado"),
     OWNER_NOT_FOUND("El propietario no ha sido encontrado "),
     INVALID_OWNER("El owner no es válido"),
     UNABLE_TO_INSTANCE("Esta clase no es posible de instanciar ");
