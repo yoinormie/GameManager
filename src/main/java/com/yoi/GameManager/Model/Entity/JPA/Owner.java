511f3de0dd1d8882f13ea6194525886572ce792f
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-03-24 11:41:40 +0100

    Relación con AppUser corregida + creado método para crear a través de una DTO

--- src/main/java/com/yoi/GameManager/Model/Entity/JPA/Owner.java
+++ src/main/java/com/yoi/GameManager/Model/Entity/JPA/Owner.java
@@ -1,9 +1,11 @@
 package com.yoi.GameManager.Model.Entity.JPA;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.yoi.GameManager.Components.AppuserDeserialzer;
+//import com.yoi.GameManager.Components.AppuserDeserialzer;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.OwnerRequests.OwnerDTOCreate;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,6 +18,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ToString
 public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,12 +34,20 @@ public class Owner {
     private String secondSurname;
 
     @JoinColumn(name = "id_user", unique = true, nullable = false)
-    @ManyToOne(optional = false, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JsonDeserialize(using = AppuserDeserialzer.class)
-    @JsonManagedReference
+    @ManyToOne(optional = false, fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JsonIgnore
     private Appuser userId;
 
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Ownership> ownershipList;
+
+    public static Owner newOwner(OwnerDTOCreate ownerDTOCreate, Appuser appuserVinculated){
+        Owner owner = new Owner();
+        owner.setName(ownerDTOCreate.getName());
+        owner.setFirstSurname(ownerDTOCreate.getFirstSurname());
+        owner.setSecondSurname(ownerDTOCreate.getSecondSurname());
+        owner.setUserId(appuserVinculated);
+        return owner;
+    }
 }
