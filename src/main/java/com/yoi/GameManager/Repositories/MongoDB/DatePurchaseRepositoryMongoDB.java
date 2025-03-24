7361b9cd1b99c49776eb69ba4250f9743a80fbf6
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-03-24 11:36:05 +0100

    Repositories de mongo innecesarios eliminados

--- src/main/java/com/yoi/GameManager/Repositories/MongoDB/DatePurchaseRepositoryMongoDB.java
+++
@@ -1,8 +0,0 @@
-package com.yoi.GameManager.Repositories.MongoDB;
-
-import com.yoi.GameManager.Model.Entity.JPA.DatePurchase;
-import com.yoi.GameManager.Model.Entity.JPA.DatePurchaseKey;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DatePurchaseRepositoryMongoDB extends MongoRepository<DatePurchase, DatePurchaseKey> {
-}
--- src/main/java/com/yoi/GameManager/Repositories/MongoDB/GameScoreRepositoryMongoDB.java
+++
@@ -1,9 +0,0 @@
-package com.yoi.GameManager.Repositories.MongoDB;
-
-import com.yoi.GameManager.Model.Entity.JPA.GameScore;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface GameScoreRepositoryMongoDB extends MongoRepository<GameScore, UUID> {
-}
