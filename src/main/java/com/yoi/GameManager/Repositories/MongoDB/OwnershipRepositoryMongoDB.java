8de30178b7b934254384bbf0ff9fd78a3f22a644
YoelGarciaLago <ygarcialago@danielcastelao.org>
2025-03-24 11:37:25 +0100

    repository innecesario eliminado

--- src/main/java/com/yoi/GameManager/Repositories/MongoDB/OwnershipRepositoryMongoDB.java
+++
@@ -1,8 +0,0 @@
-package com.yoi.GameManager.Repositories.MongoDB;
-
-import com.yoi.GameManager.Model.Entity.JPA.Ownership;
-import com.yoi.GameManager.Model.Entity.JPA.OwnershipKey;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OwnershipRepositoryMongoDB extends MongoRepository<Ownership,OwnershipKey> {
-}
