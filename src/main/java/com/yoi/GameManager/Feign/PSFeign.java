deed1f48266fcb5b35f739042fee20db8835c741
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-28 12:37:47 +0200

    Feings creados

--- src/main/java/com/yoi/GameManager/Feign/PSFeign.java
+++ src/main/java/com/yoi/GameManager/Feign/PSFeign.java
@@ -1,15 +1,20 @@
 package com.yoi.GameManager.Feign;
 
+import com.yoi.GameManager.Model.DTO.EntityDTOs.PlayStation.EnvolvedLists;
+import com.yoi.GameManager.Model.DTO.EntityDTOs.PlayStation.PlayStationGameDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.PlayStationRequest.PSGamesRequestDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.PlayStationRequest.PSUserRequestDTO;
 import com.yoi.GameManager.Model.Entity.JPA.PlayStation.UserPlaystationNetwork;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "PSFeign", url = "http://localhost:3050/playstation")
 public interface PSFeign {
     @PostMapping("/searchuserwithan")
-    UserPlaystationNetwork getUser(String username);
+    UserPlaystationNetwork getUser(@RequestBody PSUserRequestDTO requestDTO);
 
     @PostMapping("/searchgameswithaid")
-    boolean getUserGames(String accountID);
+    EnvolvedLists getUserGames(PSGamesRequestDTO requestDTO);
 
 }
--- src/main/java/com/yoi/GameManager/Feign/SteamFeign.java
+++ src/main/java/com/yoi/GameManager/Feign/SteamFeign.java
@@ -1,19 +1,23 @@
 package com.yoi.GameManager.Feign;
 
+import com.yoi.GameManager.Model.DTO.RequestDTOs.SteamRequest.SteamAchievementsRequestDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.SteamRequest.SteamGamesRequestDTO;
+import com.yoi.GameManager.Model.DTO.RequestDTOs.SteamRequest.SteamUserRequestDTO;
 import com.yoi.GameManager.Model.Entity.JPA.Steam.SteamAchievement;
 import com.yoi.GameManager.Model.Entity.JPA.Steam.SteamGame;
 import com.yoi.GameManager.Model.Entity.JPA.Steam.SteamProfile;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "SteamFeign", url = "http://localhost:3000/steam")
 public interface SteamFeign {
     @PostMapping("/getsteamiduser")
-    SteamProfile getUser(String steamID, boolean isCustom);
+    SteamProfile getUser(@RequestBody SteamUserRequestDTO steamUserRequestDTO);
 
     @PostMapping("/getuserslibrary")
-    SteamGame[] getUserGames(String steamID);
+    SteamGame[] getUserGames(@RequestBody SteamGamesRequestDTO requestDTO);
 
     @PostMapping("/makeachievementslist")
-    SteamAchievement[] getAchievementsList(String steamID, SteamGame[] ownedGames);
+    SteamAchievement[] getAchievementsList(@RequestBody SteamAchievementsRequestDTO achievementsRequestDTO);
 }
