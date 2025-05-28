951a606ed1c825ab7c1128bda9b20f0a25d6f932
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-28 12:36:41 +0200

    Archivos eliminados

--- src/main/java/com/yoi/GameManager/Model/DTO/RequestDTOs/AppuserRequests/DeleteUserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.yoi.GameManager.Model.DTO.RequestDTOs.AppuserRequests;
-
-import lombok.Data;
-
-@Data
-public class DeleteUserDTO {
-    private String username;
-    private String password;
-}
--- src/main/java/com/yoi/GameManager/Repositories/JPA/Steam/SteamGameProperyRepositoryJPA.java
+++
@@ -1,8 +0,0 @@
-package com.yoi.GameManager.Repositories.JPA.Steam;
-
-import com.yoi.GameManager.Model.Entity.JPA.Steam.GamePropertySteam;
-import com.yoi.GameManager.Model.Entity.JPA.Steam.GamepropertyId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SteamGameProperyRepositoryJPA extends JpaRepository<GamePropertySteam, GamepropertyId> {
-}
